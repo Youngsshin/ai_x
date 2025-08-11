36aff79631291487ebe0c3e3eb4ced9a9d4f4f09
youngsshin <risdudtls123@naver.com>
2025-08-11 13:23:23 +0900

    0811

---
+++ source/12_django/myproject/blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+import re
+from django.forms import ValidationError
+# Create your models here.
+
+REGION_CHOICE = (
+    ("Europe","유럽"),
+    ("Asia", "아시아"),
+    ("Oceania","오세아니아"),
+    ("America","아메리카"),
+)
+def lnglat_validator(value):
+    if not re.match(r'(\d+\.?\d*),(\d+\.?\d*)', value):
+        raise ValidationError('Invalid LngLat. ex:38, 128')
+    
+class Post(models.Model): # 테이블명 : blog_post
+    # id = models.AutoField(primary_key=True) PK가 없을 경우 자동 생성
+    title = models.CharField(verbose_name="제목",
+                            max_length=100) # VARCHAR 타입은 최대 길이 반드시 지정해야함
+    content = models.TextField(verbose_name="본문") # 최대 길이 제한 없음 CLOB, TEXT 타입
+    create_at = models.DateField(auto_now_add=True) # 자동으로 지금 시간 저장
+    update_at = models.DateTimeField(auto_now=True) 
+    region = models.CharField(verbose_name="지역",
+                            max_length=20, 
+                            choices=REGION_CHOICE,
+                            default="Asia")
+    lnglat = models.CharField(verbose_name="경도, 위도",
+                            max_length=100,
+                            blank=True,
+                            null=True,
+                            help_text="경도, 위도 포맷으로 저장", # 38.5, 125.8 / 38,125
+                            validators=[lnglat_validator])
+    url = models.URLField(blank=True,
+                        null=True)
+    
+    def __str__(self):
+        return "제목:{}-{}작성 {}최종 수정".format(self.title,
+                                                self.create_at,
+                                                self.update_at)
+    
+    class Meta:
+        ordering = ["-update_at"] # 정렬 옵션
